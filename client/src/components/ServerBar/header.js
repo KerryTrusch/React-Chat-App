1cba768afbf61e0f608a77412a531768fbe04c72
Trusch <41238059+KerryTrusch@users.noreply.github.com>
2022-08-09 02:12:52 -0400

    home button

--- client/src/components/ServerBar/header.js
+++ client/src/components/ServerBar/header.js
@@ -3,6 +3,7 @@ import './ServerButton.css';
 import CreateServer from './CreateServerModal';
 import ServerButton from './ServerButton';
 import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
 function Header({ servers, setServers, socket }) {
     const [showmod, setShowmod] = useState();
 
@@ -14,12 +15,12 @@ function Header({ servers, setServers, socket }) {
         setShowmod(false);
     }
 
-    
+
     var listOfServers;
     if (servers) {
         listOfServers = servers.map((server) =>
             <li className="liForServers" key={server.serverID}>
-                <ServerButton src={server.imgsrc} link={server.serverID} socket={socket} className="serverButton"/>
+                <ServerButton src={server.imgsrc} link={server.serverID} socket={socket} className="serverButton" />
             </li>
         );
     } else {
@@ -28,9 +29,17 @@ function Header({ servers, setServers, socket }) {
     return (
         <div>
             <ul className="ulForServers">
+                <li className='liForServers' key={"homeButton"}>
+                    <Link to={'/channels/friends'}>
+                        <img className="serverButton" alt='' src={'/discord-logo.png'} />
+                    </Link>
+                </li>
+                <div className='border-b-2 pt-3 w-2/5 m-auto border-[#373a3f]'>
+
+                </div>
                 {listOfServers}
                 <li className="liForServers">
-                    <CreateServer show={showmod} handleClose={hideModal} setServers={setServers} servers={servers}/>
+                    <CreateServer show={showmod} handleClose={hideModal} setServers={setServers} servers={servers} />
                     <button type="button" onClick={showModal}>
                         <img className="serverButton" alt='' src={'/create-plus.png'} />
                     </button>
